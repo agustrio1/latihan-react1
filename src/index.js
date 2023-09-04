ccd6231e5b4c47ff2aeaf73ea04b30a39fdac2df
agustrio1 <120178337+agustrio1@users.noreply.github.com>
2023-09-04 20:49:30 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const Button = ({ link }) => {
+  return <a href={link}>Find out more</a>;
+};
+
+const CardBody = ({ date, title, content, link }) => {
+  return (
+    <div>
+      <p>{date}</p>
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <Button link={link} />
+    </div>
+  );
+};
+const CardHeader = ({ image, category }) => {
+  return (
+    <header>
+      <h4>{category}</h4>
+      <img src={image} alt="" />
+    </header>
+  );
+};
+
+const Card = ({ image, category, date, title, content, link }) => {
+  return (
+    <article>
+      <CardHeader image={image} category={category} />
+      <CardBody date={date} title={title} content={content} link={link} />
+    </article>
+  );
+};
+
+const Header = ({ title, subtitle }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </header>
+  );
+};
+
+function News() {
+  // data news
+  const someNews = [
+    {
+      title: 'CNN Acuire BEME',
+      date: 'March 20 2022',
+      content: "CNN purchased Casey Neistat's Beme app for $25million.",
+      image: 'https://source.unsplash.com/user/erondu/600x400',
+      category: 'News',
+      link: '#'
+    },
+    {
+      title: 'React and the WP-API',
+      date: 'March 19 2022',
+      content: 'The first ever decoupled starter theme for React & the WP-API.',
+      image: 'https://source.unsplash.com/user/ilyapavlov/600x400',
+      category: 'News',
+      link: '#'
+    },
+    {
+      title: 'Nomad Lifestyle',
+      date: 'March 19 2022',
+      content: 'Learn our tips and tricks on living a nomadic lifestyle.',
+      image: 'https://source.unsplash.com/user/erondu/600x400',
+      category: 'Travel',
+      link: '#'
+    }
+  ];
+
+  // TODO: selesaikan component-nya
+  return (
+    <div>
+      <Header title="Latest News" subtitle="Covering March & April 2022" />
+      {someNews.map((news) => (
+        <Card {...news} key={news.title} />
+      ))}
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<News />);
